ddb1de92d7c39d8d4c66b05b6bc36754352c958d
Andrew Nassar <andrewnassar117@gmail.com>
2026-07-22 20:58:10 +0300

    clean project structure and remove unused files

--- src/domain/repositories/sacredStoriesRepository.ts
+++
@@ -1,6 +0,0 @@
-import { GetStoriesParams, PaginatedList, SacredStoryDetail, SacredStoryListItem } from "../entities/sacredStory";
-
-export interface ISacredStoriesRepository {
-  getStories(params?: GetStoriesParams): Promise<PaginatedList<SacredStoryListItem>>;
-  getStoryById(id: string): Promise<SacredStoryDetail>;
-}
